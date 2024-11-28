c9b116a6589859ce3480cd359d6090efef58d1e8
Hiroyuki Oiwa <oiwa.hiroyuki1@gmail.com>
2024-11-27 20:53:28 -0500

    Add requirements.txt with necessary packages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,15 +14,16 @@ extensions = [
 ]
 
 # HTML output
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-# Custom output directory for Read the Docs
-if os.getenv('READTHEDOCS', None) == 'True':
-    html_output = os.getenv('READTHEDOCS_OUTPUT', '_build/html')
-else:
-    html_output = '_build/html'
 
 # Ensure the _static directory exists
 if not os.path.exists('_static'):
     os.makedirs('_static')
+
+# Custom output directory for Read the Docs
+if os.getenv('READTHEDOCS', None) == 'True':
+    html_output = os.getenv('READTHEDOCS_OUTPUT', '_build/html')
+    html_output = os.getenv('READTHEDOCS_OUTPUT')
+else:
+    html_output = '_build/html'
